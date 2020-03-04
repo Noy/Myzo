3879340e5c48ed1a0b432bc4ddb86b13f2b074c3
noy <noyhillel@gmail.com>
2020-03-04 23:15:50 +0200

    Added pagination further option for transaction

--- monzo_test.go
+++ monzo_test.go
@@ -2,6 +2,7 @@ package myzo
 
 import (
 	"encoding/json"
+	"strings"
 	"testing"
 )
 
@@ -17,5 +18,23 @@ func TestAuthMonzo_GetAllMerchants(t *testing.T) {
 		} else {
 			t.Error("FAILED! Could not find the ID.")
 		}
+	}
+}
+
+func TestMyzo_FeedHandler(t *testing.T) {
+	auth := Myzo{ClientID:"", UserID:"", AccessToken:"", AccountID:"", Debug:true, ResponseBody: nil}
+	resp, err := auth.FeedHandler("https://google.com", []string{
+		"This will be the notification on your app!",
+		"Link to url",
+		"#000000",
+		"#000000",
+		"#000000",
+		"Body text",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	if strings.Contains(string(resp), "bad") {
+		t.Error("Did not do it correctly, check all is in order: " + string(resp))
 	}
 }
--- transactions.go
+++ transactions.go
@@ -1,26 +1,26 @@
 package myzo
 
-func bulkTransactionsRequest(auth *Myzo, daysAgo int, expandBy, limit string) *TransactionsResponse {
-	r, _ := auth.transactionResponseHandler(true, daysAgo, expandBy, "", limit)
+func bulkTransactionsRequest(auth *Myzo, daysAgo,before int, expandBy, limit string) *TransactionsResponse {
+	r, _ := auth.transactionResponseHandler(true, daysAgo, before, expandBy, "", limit)
 	return r
 }
 
 func baseTransactionRequest(auth *Myzo, expandBy, optionalId string) *TransactionsResponse {
-	r, _ := auth.transactionResponseHandler(false,0, expandBy, optionalId, "")
+	r, _ := auth.transactionResponseHandler(false,0,0, expandBy, optionalId, "")
 	return r
 }
 
-func (auth *Myzo) GetAllTransactions(daysAgo int, expandBy, limit string) []Transaction {
-	return bulkTransactionsRequest(auth, daysAgo, expandBy, limit).Transactions
+func (auth *Myzo) GetAllTransactions(daysAgo, before int, expandBy, limit string) []Transaction {
+	return bulkTransactionsRequest(auth, daysAgo, before, expandBy, limit).Transactions
 }
 
 func (auth *Myzo) GetTransaction(id, expandBy string) Transaction {
 	return baseTransactionRequest(auth, expandBy, "/"+id).Transaction
 }
 
-func (auth *Myzo) GetAllMerchants(daysAgo int, limit string) []Merchant {
+func (auth *Myzo) GetAllMerchants(daysAgo, before int, limit string) []Merchant {
 	var merchants []Merchant
-	for _, t := range bulkTransactionsRequest(auth, daysAgo, "merchant", limit).Transactions {
+	for _, t := range bulkTransactionsRequest(auth, daysAgo, before, "merchant", limit).Transactions {
 		merchants = append(merchants, t.Merchant)
 	}
 	return merchants
